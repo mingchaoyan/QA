5ef74e3bec494bda283a88c795194fe9e016d6fc
mingchaoyan <mingchaoyan@gmail.com>
2016-04-20 15:15:45 +0800

    model

---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Model : MonoBehaviour
+{
+    public Question[] _question;
+
+}
---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Question : MonoBehaviour
+{
+    public string question;
+    public string[] answer;
+    public int correct;
+    public string right;
+    public string wrong;
+
+}
