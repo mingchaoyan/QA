c1dc85c791ab0e3ea5673932fbf55574e859f593
mingchaoyan <mingchaoyan@gmail.com>
2016-04-20 21:43:23 +0800

    添加GDGeek工具

---
+++ Assets/Scripts/Ctrl.cs
@@ -0,0 +1,148 @@
+﻿using UnityEngine;
+using System.Collections;
+using GDGeek;
+
+public class Ctrl : MonoBehaviour
+{
+    public Model _model;
+    public View _view;
+    private FSM _fsm = new FSM ();
+    private int _score = 0;
+    private int _index = 0;
+    private bool _correct = false;
+
+    public void fsmPost (string msg)
+    {
+        _fsm.post (msg);		
+    }
+
+    private State beginState ()
+    {
+        StateWithEventMap begin = new StateWithEventMap ();
+        begin.onStart += delegate {
+            _index = 0;
+            _view._begin.SetActive (true);
+        };
+        begin.onOver += delegate {
+            _view._begin.SetActive (false);
+        };
+			
+        begin.addAction ("OK", delegate {
+            return "show";
+        });
+        return begin;
+    }
+
+    private void result (int select)
+    {
+        Question q = _model._question [_index];
+        if (select == q.correct) {
+            _correct = true;
+        } else {
+            _correct = false;			
+        }
+    }
+
+    private State showState ()
+    {
+        StateWithEventMap show = new StateWithEventMap ();
+        show.onStart += delegate {
+            Question q = _model._question [_index];
+            _view._questionText.text = q.question;
+            _view._aText.text = q.answer [0];
+            _view._bText.text = q.answer [1];
+            _view._cText.text = q.answer [2];
+            _view._dText.text = q.answer [3];
+        };
+			
+        show.addAction ("A", delegate {
+            result (0);
+            return "result";
+        });
+        show.addAction ("B", delegate {
+            result (1);
+            return "result";
+        });
+        show.addAction ("C", delegate {
+            result (2);
+            return "result";
+        });
+        show.addAction ("D", delegate {
+            result (3);
+            return "result";
+        });
+			
+			
+        return show;
+    }
+
+		
+    private State resultState ()
+    {
+        StateWithEventMap result = new StateWithEventMap ();
+        result.onStart += delegate {
+            _view._result.transform.localScale = Vector3.zero;
+				
+            TweenScale tween = TweenScale.Begin (_view._result.gameObject, 0.3f, Vector3.one);
+            tween.method = Tween.Method.easeOutBounce;
+				
+				
+            Question q = _model._question [_index];
+            if (_correct) {
+                ++_score;
+                _view._resultText.text = q.right;
+                _view._result.SetActive (true);
+            } else {
+					
+                _view._resultText.text = q.wrong;
+                _view._result.SetActive (true);
+            }
+        };
+        result.onOver += delegate {
+            _view._result.SetActive (false);
+        };
+			
+        result.addAction ("OK", delegate {
+            _index++;
+            if (_index >= _model._question.Length) {
+                return "over";
+            } else {
+                return "show";
+            }
+				
+        });
+        return result;
+    }
+
+    private State overState ()
+    {
+        StateWithEventMap over = new StateWithEventMap ();
+			
+        over.onStart += delegate {
+            _view._scoreText.text = _score.ToString ();
+            _view._end.SetActive (true);
+        };
+        over.onOver += delegate {
+				
+            this._index = 0;
+            _score = 0;
+            _view._end.SetActive (false);
+        };
+			
+        over.addAction ("OK", delegate {
+            return "begin";
+        });
+        return over;
+    }
+
+    void Start ()
+    {
+        _fsm.addState ("begin", beginState ());
+        _fsm.addState ("show", showState ());
+        _fsm.addState ("result", resultState ());
+        _fsm.addState ("over", overState ());
+        _fsm.init ("begin");
+    }
+		
+		
+}
