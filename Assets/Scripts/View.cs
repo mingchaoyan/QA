570452417f6b96d02ccca1a2b5ba583fa359b3df
mingchaoyan <mingchaoyan@gmail.com>
2016-04-20 20:52:31 +0800

    完成视图层代码

---
+++ Assets/Scripts/View.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class View : MonoBehaviour
+{
+    public Text _questionText;
+    public Text _aText;
+    public Text _bText;
+    public Text _cText;
+    public Text _dText;
+    public Text _resultText;
+    public Text _scoreText;
+
+    public GameObject _begin;
+    public GameObject _result;
+    public GameObject _end;
+    public GameObject _play;
+
+}
