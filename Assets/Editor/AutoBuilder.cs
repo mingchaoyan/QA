4d07eff0ca38ac747bb46f5c5408347646ad74cb
mingchaoyan <mingchaoyan@gmail.com>
2016-05-10 14:00:29 +0800

    删去不必要的文件

---
+++ Assets/Editor/AutoBuilder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public static class AutoBuilder {
+	static BuildOptions AutoBuildOption = BuildOptions.None;
+
+	static string GetProjectName() {
+		string[] s = Application.dataPath.Split('/');
+		return s[s.Length-2];
+	}
+
+	static string[] GetScenePaths() {
+		var scenes = new string[EditorBuildSettings.scenes.Length];
+		for(int i = 0; i < scenes.Length; ++i) {
+			var scene = EditorBuildSettings.scenes[i];
+			if(scene.enabled) {
+				scenes[i] = scene.path;
+			}
+			
+		}
+		return scenes;
+	}
+
+	[MenuItem("File/AutoBuilder/Android")]
+	static void PerformAndroidBuild() {
+		Debug.Log("~/GitHub/QA/Build/" + GetProjectName() + ".apk");
+		EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget.Android);
+		BuildPipeline.BuildPlayer(GetScenePaths(), "Build/" + GetProjectName() + ".apk", BuildTarget.Android, AutoBuildOption );
+	}
+}
--- compile.sh
+++
@@ -1,1 +0,0 @@
-gmcs -target:library -out:Assets/Resources/Assembly-CSharp.bytes -debug- -recurse:*.cs -reference:/Applications/Unity/Unity.app/Contents/Frameworks/Managed/UnityEngine.dll,/Applications/Unity/Unity.app/Contents/UnityExtensions/Unity/GUISystem/UnityEngine.UI.dll,/Applications/Unity/Unity.app/Contents/Frameworks/Managed/UnityEditor.dll
